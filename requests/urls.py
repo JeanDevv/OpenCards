cbd25e0d926a095898fc0287adeb13f8792dd6f8
JeanDevv <cjean8164@gmail.com>
2024-01-18 07:59:10 -0300

    Startapp created and with request files

---
+++ requests/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from requests.views import home, pedidos, status_pedidos
+
+
+#   dominio.com/requests/home
+urlpatterns = [
+    path('', home), # Home
+    path('pedidos/', pedidos), #Pedidos
+    path('status_pedidos/', status_pedidos) #Status do Pedido
+]
--- requests/views.py
+++ requests/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('HOME 2')
+
+def pedidos(request):
+    return HttpResponse('PEDIDOS 2')
+
+def status_pedidos(request):
+    return HttpResponse('STATUS PEDIDOS 2')
